54e9186ca9a6aeadd36035116916c2813534f18b
Damir <dusenbai0064@gmail.com>
2026-09-20 21:30:14 +0500

    feat: implement divide and conquer algorithms and save benchmark results

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.example.benchmark.Benchmark;
+
+public final class Main {
+    private Main() {
+    }
+
+    public static void main(String[] args) {
+        Benchmark.main(args);
+    }
+}
